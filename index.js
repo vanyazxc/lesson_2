bba0a221570ef57a87047ea1e3b1d340c675f221
vanyazxc <t9pheeh@gmail.com>
2025-02-10 14:42:25 +0300

    Add get Endpoint

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.get('/info', (req, res) => {
+    res.json({ surname: 'Овсянников', group: '9ИС-421' });
+});
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
