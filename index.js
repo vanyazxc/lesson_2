ec04af3d5b4041bb7cb3c1ae966bac9bddb2dbc5
vanyazxc <t9pheeh@gmail.com>
2025-02-10 12:00:31 +0300

    Add get Endpoint

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const { Sequelize } = require('sequelize');
 const app = express();
 const PORT = 8080;
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'database.sqlite'
-});
-app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-app.get('/info', (req, res) => {
-    res.json({ surname: 'Овсянников', group: '9ИС-421' });
-  });
+app.get('/info', (req, res) => {       
+  res.json({ surname: 'Овсянников', group: '9ИС-421' });     });     
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на http://localhost:${PORT}`);
+  console.log(`Сервер запущен на http:localhost:${PORT}`);
 });
