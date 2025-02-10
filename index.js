716f0801411b46a492de5af42d07f63a95165d5b
vanyazxc <t9pheeh@gmail.com>
2025-02-10 12:03:47 +0300

    Add web server

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const PORT = 8080;
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-app.get('/info', (req, res) => {       
-  res.json({ surname: 'Овсянников', group: '9ИС-421' });     });     
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http:localhost:${PORT}`);
 });
