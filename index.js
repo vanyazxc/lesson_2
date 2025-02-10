7ee7c733dd8a35a6dbee484e8b22603b3989584f
vanyazxc <t9pheeh@gmail.com>
2025-02-10 12:05:56 +0300

    Add Post Endpoint

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'database.sqlite'
+});
+const Record = sequelize.define('Record', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
+sequelize.sync()
+  .then(() => console.log('База данных синхронизирована'))
+  .catch(err => console.error('Ошибка синхронизации базы данных:', err));
+app.post('/records', async (req, res) => {
+  try {
+    const record = await Record.create(req.body);
+    res.status(201).json(record);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World');
